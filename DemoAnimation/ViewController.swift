eae8cedc1bcfbd76ac22efb1523f398680571d8f
Vimal Das <vimal@Vimals-Mac-mini.local>
2017-11-17 23:16:23 +0530

    Initial Commit

---
+++ DemoAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoAnimation
+//
+//  Created by Vimal Das on 17/11/17.
+//  Copyright © 2017 Vimal Das. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
